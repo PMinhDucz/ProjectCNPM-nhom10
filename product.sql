f8c0e80f21c8b8e5bcf62418230700aa1c7cca75
Lê Duy Thành <152592360+thanhlee0801@users.noreply.github.com>
2024-05-03 22:05:10 +0700

    Add files via upload

---
+++ product.sql
@@ -0,0 +1,34 @@
+/*
+-- Query: 
+-- Date: 2023-12-22 16:58
+*/
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP001', 'Rau muống sạch', 'R0001', 35, 40, 'kg', 'Rau muống sạch nhà trồng', NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP002', 'Cà phê Trung Nguyên', 'CF001', 43, 45, 'hộp', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP003', 'Nước mắm Chin-su ớt tỏi Lý Sơn', 'NN001', 13, 37, 'chai', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP004', 'Nước mắm Chin-su me tươi Nam Bộ', 'NN001', 35, 37, 'chai', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP005', 'Nước mắm Chin-su cá cơm Biển Đông', 'NN001', 42, 37, 'chai', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP006', 'Cà phê sữa Highland Coffee', 'CF001', 43, 41, 'hộp', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP007', 'Cải thảo Hàn Quốc', 'R0001', 34, 45, 'kg', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP008', 'Mỳ tôm Kokomi 90', 'MT001', 123, 12, 'gói', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP009', 'Mỳ tôm Thanh long Caty', 'MT001', 143, 14, 'gói', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP010', 'Mỳ tôm Kokomi đại 1 gói rưỡi', 'MT001', 152, 13, 'gói', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP011', 'Cà phê Star Bucks', 'CF001', 112, 33, 'hộp', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP012', 'Nước mắm Đệ Nhị nhãn vàng', 'NN001', 33, 36, 'chai', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP013', 'Bánh đồng xu 10 won', 'AV001', 24, 20, 'cái', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP014', 'Bánh mỳ thập cẩm', 'AV001', 23, 18, 'cái', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP015', 'Trà chanh giã tay', 'CF001', 43, 15, 'hộp', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP016', 'Cà tím', 'R0001', 26, 26, 'kg', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP017', 'Chuối tươi', 'R0001', 23, 30, 'kg', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP018', 'Mỳ tương đen Koreno Chajang', 'MT001', 233, 11, 'gói', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP019', 'Sữa tươi Vinamilk nguyên chất có đường', 'CF001', 130, 22, 'lốc', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP020', 'Trà sữa trân châu đường đen', 'CF001', 223, 17, 'hộp', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP021', 'Táo xanh Nam Mỹ', 'R0001', 213, 30, 'kg', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP022', 'Mỳ tôm Hảo Hảo', 'MT001', 310, 10, 'gói', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP023', 'Bánh kem cá ', 'AV001', 230, 10, 'chiếc', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP024', 'Bánh tráng trộn', 'AV001', 222, 12, 'gói', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP025', 'Xiên que các loại', 'AV001', 203, 5, 'cái', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP026', 'Trà ô long TEA+', 'CF001', 34, 23, 'chai', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP027', 'Nước mắm Chin-su nấm shintake', 'NN001', 22, 39, 'chai', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP028', 'Dứa tươi', 'R0001', 23, 43, 'kg', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP029', 'Trà xanh matcha Birdy', 'CF001', 33, 30, 'gói', NULL, NULL);
+INSERT INTO `products` (`id`, `product`, `category_id`, `remain`, `price`, `unit`, `context`, `img_link`) VALUES ('SP030', 'Cam sành Cao Phong', 'R0001', 35, 22, 'kg', NULL, NULL);
